b5155b9a7f4886d2f881877d70cebf2483adbc66
lordeiro-dias <eduborgesbldias@gmail.com>
2026-09-16 10:46:11 -0300

    first commit

---
+++ MVC-Biblioteca/MVC-Biblioteca/Data/AppDbContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using MVC_Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Biblioteca.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Livro> Livros { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:ConexaoPadrao");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Livro>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Livro");
+
+            entity.Property(e => e.Autor)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.DataLancamento).HasPrecision(0);
+            entity.Property(e => e.Descricao).IsUnicode(false);
+            entity.Property(e => e.Titulo)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.UsuarioID).HasName("PK__Usuario__2B3DE798EA4C7A9B");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Nome)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Senha).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MVC-Biblioteca/MVC-Biblioteca/Models/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_Biblioteca.Models;
+
+public partial class Livro
+{
+    public string Titulo { get; set; } = null!;
+
+    public string Autor { get; set; } = null!;
+
+    public DateTime DataLancamento { get; set; }
+
+    public string Descricao { get; set; } = null!;
+
+    public byte[]? Imagem { get; set; }
+}
